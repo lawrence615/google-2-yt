d40d037971b09223c267811c88cf4f08589153cc
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-03 09:47:26 +0300

    giving permission to fetch images from unsplash.com and google.co.uk

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname:'images.unsplash.com',
+        port:'',
+        pathname:'/photo-**'
+      },
+      {
+        protocol: 'https',
+        hostname:'www.google.co.uk',
+        port:'',
+        pathname:'/images/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
