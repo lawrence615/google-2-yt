1d5573719e374cfc6135cc2531e5cb6e01ef2e62
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-09 11:45:57 +0300

    Used the HeaderOption component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,8 +1,32 @@
+import {
+  MagnifyingGlassIcon,
+  PhotoIcon,
+  PlayCircleIcon,
+  NewspaperIcon,
+  MapIcon,
+  EllipsisVerticalIcon,
+} from "@heroicons/react/24/outline";
+import HeaderOption from "./HeaderOption";
 
 function HeaderOptions() {
   return (
-    <div>HeaderOptions</div>
-  )
+    <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]">
+      {/** Left */}
+      <div className="flex space-x-6">
+        <HeaderOption Icon={MagnifyingGlassIcon} title="All" selected />
+        <HeaderOption Icon={PhotoIcon} title="Images" />
+        <HeaderOption Icon={PlayCircleIcon} title="Videos" />
+        <HeaderOption Icon={NewspaperIcon} title="News" />
+        <HeaderOption Icon={MapIcon} title="Maps" />
+        <HeaderOption Icon={EllipsisVerticalIcon} title="More" />
+      </div>
+      {/** Right */}
+      <div className="flex space-x-4">
+        <p className="link">Settings</p>
+        <p className="link">Tools</p>
+      </div>
+    </div>
+  );
 }
 
-export default HeaderOptions
+export default HeaderOptions;
