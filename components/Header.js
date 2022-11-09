9826cf88d82a3cd948d76e01fcdd3b85f7ea85b0
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-09 10:08:54 +0300

    Almost done with the header, working on the lower section of the header AKA HeaderOptions

---
+++ components/Header.js
@@ -0,0 +1,60 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { useRef } from "react";
+import {
+  MagnifyingGlassIcon,
+  MicrophoneIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import Avatar from "./Avatar";
+import HeaderOptions from "./HeaderOptions";
+
+function Header() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={40}
+          width={120}
+          className="cursor-pointer"
+          alt="Google Logo"
+          onClick={() => router.push("/")}
+        />
+        <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            type="text"
+          />
+          <XMarkIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <MagnifyingGlassIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar url="https://images.unsplash.com/photo-1575147724231-f759a1e41b5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dm9sdm98ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="ml-auto"  />
+      </div>
+      {/** HeaderOptions */}
+      <HeaderOptions/>
+    </header>
+  );
+}
+
+export default Header;
