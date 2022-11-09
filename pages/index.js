3a77679ebbb957db081a6d0707baf416d7d6a0c9
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-09 10:10:31 +0300

    once any of the buttons are clicked, the search term is passed to "/search?term=value"

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
 import {
@@ -11,7 +13,21 @@ import {
 import Avatar from "../components/Avatar";
 import Footer from "../components/Footer";
 
+
 export default function Home() {
+
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value
+
+    if(!term) return
+
+    router.push(`/search?term=${term}`)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -47,12 +63,12 @@ export default function Home() {
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <MagnifyingGlassIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-          <button className="btn">Google Search</button>
-          <button className="btn">I'm Feeling Lucky</button>
+          <button onClick={search} className="btn">Google Search</button>
+          <button onClick={search} className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
 
