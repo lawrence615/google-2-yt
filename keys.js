2a3f2b838e01672dfe4a4021c77e3bf23cd81885
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-10 09:42:48 +0300

    initial commit
    API_KEY and CONTEXT_KEY values fetched from .env file

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - makes it possible to use the Google API - https://developers.google.com/custom-search/v1/using_rest/api_key
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = process.env.NEXT_PUBLIC_CONTEXT_KEY
