1357c8dc3c6364ff1b1d5a477a4f9f5f48f7f507
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-11-11 09:06:45 +0300

    mocking search request

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,14 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-
 import Header from "../components/Header";
 import SearchResults from "../components/SearchResults";
 import { API_KEY, CONTEXT_KEY } from "../keys";
-import response from "../response";
+// import response from "../response";
 
-function Search({results}) {
-  const router = useRouter()
-  console.log(results)
+function Search({ results }) {
+  const router = useRouter();
+  console.log(router.query);
   return (
     <div>
       <Head>
@@ -17,7 +16,7 @@ function Search({results}) {
       </Head>
       <Header />
       <SearchResults results={results} />
-      </div>
+    </div>
   );
 }
 
@@ -25,11 +24,15 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useDummyData = true;
-  const startIndex = context.query.start || '0'
+  const startIndex = context.query.start || "0";
 
-  const data = useDummyData ? response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
-  ).then((response) => response.json());
+  const data = useDummyData
+    ? await fetch("http://localhost:3000/api/search").then((response) =>
+        response.json()
+      )
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
 
   /** After the server has rendered... Pass the results to the client ... */
   return {
